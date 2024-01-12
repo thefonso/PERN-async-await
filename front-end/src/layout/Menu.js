322167f204b4ccfc436010955459ca733d8d7ecc
thefonso <unclefonso@protonmail.com>
2024-01-12 10:00:56 -0500

    minor adjusting of css

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -16,25 +16,25 @@ function Menu() {
         <li className="top-nav-item">
           <Link className="light med-neutral-hover" to="/dashboard">
             <i className="nav-icon fas fa-tachometer-alt dark-neutral"></i>
-            <span className="menu-text"> Dashboard</span>
+            <span className="menu-text">Dashboard</span>
           </Link>
         </li>
         <li className="top-nav-item">
           <Link className="light med-neutral-hover" to="/search">
             <i className="nav-icon fas fa-search dark-neutral"></i>
-            <span className="menu-text"> Search</span>
+            <span className="menu-text">Search</span>
           </Link>
         </li>
         <li className="top-nav-item">
           <Link className="light med-neutral-hover" to="/reservations/new">
             <i className="nav-icon fas fa-user-plus dark-green"></i>
-            <span className="menu-text"> New Reservation</span>
+            <span className="menu-text">New Reservation</span>
           </Link>
         </li>
         <li className="top-nav-item">
           <Link className="light med-neutral-hover" to="/tables/new">
             <i className="nav-icon fas fa-utensils dark-green"></i>
-            <span className="menu-text"> New Table</span>
+            <span className="menu-text">New Table</span>
           </Link>
         </li>
       </ul>
